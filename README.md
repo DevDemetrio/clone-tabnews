47d3553b13e4f1fd292f1df985f241c3a798a3ee
Demétrio do Nascimento Lopes <devdemetrio@gmail.com>
2024-09-09 18:41:50 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implesmentação do https://www.tabnews.com.br para o https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Melinda linda</h1>
+function Home() {
+  return <h1>Melinda linda</h1>;
 }
 
-export default Home
+export default Home;
